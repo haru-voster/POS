18657b618c137c6306c0db59d7fe6971e97964d7
Haroun wabwire <wabwireharoun2001@gmail.com>
2025-09-19 18:32:08 +0300

    connf

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SHOP_NAME = os.getenv("SHOP_NAME", "Scurite Restaurant POS")
+SHOP_NAME = os.getenv("SHOP_NAME", "Scurite MAMA LIAM FRIES")
 SHOP_MOBILE = os.getenv("SHOP_MOBILE", "0000000000")
 CURRENCY = os.getenv("CURRENCY", "Kshs.")
 DB_PATH = os.getenv("DB_PATH", "db/database.db")
